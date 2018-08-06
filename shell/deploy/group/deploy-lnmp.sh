319fcba1bdf65d6c24f1fbf609e3d950eeb5074f
李烈华 <71920204@qq.com>
2018-08-06 23:15:04 +0800

    增加nginx的server_name自定义配置

--- shell/deploy/group/deploy-lnmp.sh
+++ shell/deploy/group/deploy-lnmp.sh
@@ -5,7 +5,15 @@ baseDir="$currentDir"/../../..;
 
 echo '--------------LNMP组合部署项目--------------';
 echo;
+serverName=`curl icanhazip.com`
 read -p '请输入待部署的项目目录：' projectDir
+read -p "请输入服务器名，不填默认为本机公网IP($serverName)：" tempServerName
+
+if ! test -z ${tempServerName}
+then
+    serverName=${tempServerName};
+fi
+
 echo;
 projectName=$(echo `basename "$projectDir"`);
 
@@ -15,19 +23,20 @@ then
     mkdir -p "$baseDir"/_output/config/nginx/
     cp "$baseDir"/config/nginx/server.conf "$_"/"$targetFileName"
     sed -i "s/\${projectName}/${projectName}/g" "$baseDir"/_output/config/nginx/"$targetFileName"
+    sed -i "s/\${serverName}/${serverName}/g" "$baseDir"/_output/config/nginx/"$targetFileName"
 
     cp "$baseDir"/docker-compose/lnmp.yml "$baseDir"/_output/docker-compose.yml
     sed -i "s#\${projectName}#${projectName}#g" "$baseDir"/_output/docker-compose.yml
     sed -i "s#\${projectDir}#${projectDir}#g" "$baseDir"/_output/docker-compose.yml
     sed -i "s#\${baseDir}#${baseDir}#g" "$baseDir"/_output/docker-compose.yml
 
-    echo "127.0.0.1      local-$projectName.com" >> /etc/hosts
-    echo "::1            local-$projectName.com" >> /etc/hosts
+    echo "127.0.0.1      $serverName" >> /etc/hosts
+    echo "::1            $serverName" >> /etc/hosts
 
-    echo -e "\033[32m 部署成功！项目访问地址：http://local-${projectName}.com:81 \033[0m"
+    echo -e "\033[32m 部署成功！项目访问地址：http://$serverName:666 \033[0m"
     echo "Mac系统请输入sudo vi /etc/hosts，在hosts文件中补充如下代码"
-    echo "127.0.0.1      local-$projectName.com";
-    echo "::1            local-$projectName.com";
+    echo "127.0.0.1      $serverName";
+    echo "::1            $serverName";
 else
     echo -e "\033[31m 请输入正确的项目目录！ \033[0m"
 fi
